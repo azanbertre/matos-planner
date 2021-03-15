68983e1102f68ad37b93b172b4d67880e729dd4e
Pedro Borges <pedro.hov.borges@gmail.com>
2021-03-15 01:52:41 -0300

    update home and appbar

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,56 +1,52 @@
 <template>
-  <v-app>
-    <Appbar/>
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
+    <v-app>
+        <Appbar/>
+        <v-main>
+            <router-view />
+        </v-main>
+    </v-app>
 </template>
 
 <script>
 import Appbar from "@/components/Appbar.vue"
 import './static/main.css'
 export default {
-  name: "App",
+    name: "App",
 
-  components: {
-    Appbar
-  },
-
-  computed: {
-    authenticated() {
-      return this.$store.getters.token !== '';
+    components: {
+        Appbar
     },
-  },
 
-  watch: {
-    authenticated() {
-      if (!this.authenticated) {
-        this.$router.replace("/login");
-      }
-    }
-  },
-
-  mounted() {
-    this.checkAuth();
-  },
-
-  methods: {
-    checkAuth(){
-      if (this.$store.getters.token === '') {
-        this.$store.dispatch('anonymous');
-      } else {
-        this.$store.dispatch('refresh');
-      }
+    computed: {
+        authenticated() {
+            return this.$store.getters.token !== '';
+        },
     },
-    logout() {
-      this.$store.commit('logout');
-      this.$router.push("/login");
-    }
-  },
 
-  data: () => ({
-    //
-  })
+    watch: {
+        authenticated() {
+            if (!this.authenticated) {
+                this.$router.replace("/login");
+            }
+        }
+    },
+
+    mounted() {
+            this.checkAuth();
+    },
+
+    methods: {
+        checkAuth(){
+            this.$store.dispatch('refresh');
+        },
+        logout() {
+            this.$store.commit('logout');
+            this.$router.push("/login");
+        }
+    },
+
+    data: () => ({
+        //
+    })
 };
 </script>
--- client/src/components/Appbar.vue
+++ client/src/components/Appbar.vue
@@ -1,46 +1,36 @@
 <template>
-  <div v-if="!isGamePage()">
-    <v-app-bar color="white" light>
-      <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+    <div>
+        <v-app-bar color="white" light>
+            <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
 
-      <v-toolbar-title class="title pl-10"
-        ><span class="font-weight-bold">Smart</span>
-        <span class="font-weight-light">Math</span></v-toolbar-title
-      >
-      <v-spacer></v-spacer>
-      <!-- <v-btn icon class="" v-if="isAuthenticated" @click="$store.commit('logout')">
-        <v-icon>mdi-logout-variant</v-icon>
-      </v-btn> -->
-    </v-app-bar>
-  </div>
+            <v-toolbar-title class="title pl-10">
+                <span class="font-weight-bold">Matos</span>
+                <span class="font-weight-light">Planner</span>
+            </v-toolbar-title>
+
+            <v-spacer></v-spacer>
+            <!-- <v-btn icon class="" v-if="isAuthenticated" @click="$store.commit('logout')">
+                    <v-icon>mdi-logout-variant</v-icon>
+            </v-btn> -->
+        </v-app-bar>
+    </div>
 </template>
 <style>
-  .title {
-    color: #0077ff;
-  }
+    .title {
+        color: #33395E;
+    }
 </style>
 <script>
-  export default {
-    data(){
-      return{
-        isAuthenticated: true,
-      }
-    },
-    methods: {
-      isGamePage(){
-        const path = this.$router.currentRoute.path
-        return this.$router.currentRoute.name == "Game"
-      },
-      isRoomPage(){
-        const path = this.$router.currentRoute.path
-        return this.$router.currentRoute.path.includes("/turma")
-      },
-      navigateTabs(tab) {
-        const path = this.$router.currentRoute.path
-        if (tab !== path) return this.$router.push(tab);
-      },
-    },
-  };
+    export default {
+        data(){
+            return{
+                isAuthenticated: true,
+            }
+        },
+        methods: {
+
+        },
+    };
 </script>
 
 <style></style>
