55618b4f573504058a5322a9a1039e33118ca766
Pedro Borges <pedro.hov.borges@gmail.com>
2021-03-15 21:18:39 -0300

    add global snackbar

---
+++ client/src/components/SnackBar.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-snackbar v-model="$store.getters.info.show" timeout="3000" :color="color" top>
+
+        {{ message }}
+
+        <template v-slot:action="{ attrs }">
+            <v-btn
+                text
+                v-bind="attrs"
+                @click="$store.state.info.show = false"
+            >
+                Fechar
+            </v-btn>
+
+        </template>
+
+    </v-snackbar>
+</template>
+<style>
+
+</style>
+<script>
+    export default {
+        data() {
+            return {}
+        },
+        computed: {
+            message() {
+                return this.$store.getters.info.message;
+            },
+            color() {
+                return this.$store.getters.info.color;
+            }
+        },
+        methods: {
+
+        },
+    };
+</script>
+
+<style></style>
